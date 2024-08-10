13b59f94b786517e5ea2c0dc6aad66251b932cb9
Hayeonoct <octopuscurry@naver.com>
2024-08-11 00:37:35 +0900

    App.js 수정

--- BackEnd/pybo/frontend/src/App.js
+++ BackEnd/pybo/frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';  // GoogleOAuthProvider 임포트
 import StartPage from './components/StartPage';
 import SignUp from './components/SignUp';
-import ChatbotPage from './components/ChatbotPage'; // ChatbotPage 컴포넌트 임포트
+import ChatbotPage from './components/ChatbotPage';
 import Login from './components/Login';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/chatbot" element={<ChatbotPage />} /> {/* ChatbotPage 라우트 */}
-      </Routes>
-    </Router>
+    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID"> {/* clientId를 입력하세요 */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/chatbot" element={<ChatbotPage />} />
+        </Routes>
+      </Router>
+    </GoogleOAuthProvider>
   );
 }
 
